8fab7a1162bbcd5e565d1b3160bca856e64b5fb5
kuizuo <kuizuo12@163.com>
2022-07-10 23:58:43 +0800

    fix: 优化区分系统代码

--- src/core/openBlog.ts
+++ src/core/openBlog.ts
@@ -1,8 +1,6 @@
 import * as vscode from 'vscode'
 import { exec } from 'child_process'
-
-const workspacePath = vscode.workspace.workspaceFolders?.[0].uri.path ?? ''
-const isWindows = /^\/.:\//.test(workspacePath)
+import * as os from 'os'
 
 export async function activate(context?: vscode.ExtensionContext) {
   const key = 'kuizuoPlugin.showTip'
@@ -10,7 +8,7 @@ export async function activate(context?: vscode.ExtensionContext) {
   if (showTip) {
     const result = await vscode.window.showInformationMessage('是否要打开愧怍的小站？', '是', '否', '不再提示')
     if (result === '是') {
-      const commandLine = isWindows ? `start https://kuizuo.cn` : `open https://kuizuo.cn`
+      const commandLine = os.platform() === 'win32' ? `start https://kuizuo.cn` : `open https://kuizuo.cn`
       exec(commandLine)
     } else if (result === '不再提示') {
       //最后一个参数，为true时表示写入全局配置，为false或不传时则只写入工作区配置

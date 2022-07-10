b09a8e9c80c622d21bb2388b07ea543c36ad4cca
kuizuo <kuizuo12@163.com>
2022-07-10 19:33:35 +0800

    feat: 自定义扩展工作台

---
+++ src/core/statusBar.ts
@@ -0,0 +1,41 @@
+import * as vscode from 'vscode'
+
+let myStatusBarItem: vscode.StatusBarItem
+
+export function activate(context: vscode.ExtensionContext) {
+  const myCommandId = 'kuizuo-plugin.showStatusBarItem'
+  context.subscriptions.push(
+    vscode.commands.registerCommand(myCommandId, () => {
+      const n = getNumberOfSelectedLines(vscode.window.activeTextEditor)
+      vscode.window.showInformationMessage(`Yeah, ${n} line(s) selected... Keep going!`)
+    }),
+  )
+
+  myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100)
+  myStatusBarItem.command = myCommandId
+  context.subscriptions.push(myStatusBarItem)
+  myStatusBarItem.show()
+  context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem))
+  context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem))
+
+  updateStatusBarItem()
+}
+
+function updateStatusBarItem(): void {
+  const n = getNumberOfSelectedLines(vscode.window.activeTextEditor)
+  if (n > 0) {
+    myStatusBarItem.text = `$(megaphone) ${n} line(s) selected`
+    myStatusBarItem.tooltip = `Yeah, ${n} line(s) selected... Keep going!`
+    myStatusBarItem.show()
+  } else {
+    myStatusBarItem.hide()
+  }
+}
+
+function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
+  let lines = 0
+  if (editor) {
+    lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0)
+  }
+  return lines
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,17 @@
 import * as vscode from 'vscode'
 import { activate as openBlog } from './core/openBlog'
-import newFile from './core/newFile'
+import { activate as newFile } from './core/newFile'
+import { activate as statusBar } from './core/statusBar'
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('扩展“kuizuo-plugin”已被激活！')
   console.log(vscode)
 
-  openBlog()
-  
-  context.subscriptions.push(newFile)
-  
+  openBlog(context)
+  newFile(context)
+
+  statusBar(context)
+
   let disposable = vscode.commands.registerCommand('kuizuo-plugin.helloWorld', () => {
     vscode.window.showInformationMessage('Hello World from kuizuo-plugin!')
   })

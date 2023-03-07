29895bf294819cc3a347604c7e305cdc3d0b06c5
kuizuo <hi@kuizuo.cn>
2023-03-07 18:47:45 +0800

    chore: remove newFile

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode'
 import { activate as openBlog } from './core/openBlog'
-import { activate as newFile } from './core/newFile'
 import { activate as statusBar } from './core/statusBar'
 import { activate as helloWorld } from './core/helloWorld'
 import { activate as hover } from './core/hover'
@@ -8,11 +7,9 @@ import { activate as webview } from './core/webview'
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('扩展“vscode-kuizuo”已被激活！')
-  console.log(vscode)
 
   helloWorld(context)
   openBlog(context)
-  newFile(context)
 
   statusBar(context)
   hover(context)
